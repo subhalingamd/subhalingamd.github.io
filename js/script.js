324135a9abefcc892b456a581e5ac74efba9ceae
Subhalingam D <subhalingam.d@gmail.com>
2021-07-25 18:50:29 +0530

    [dev] typing effect for role in cover

--- js/script.js
+++ js/script.js
@@ -146,7 +146,70 @@
 
     $('[data-url]').on("click",function() { /*window.location = $(this).data('url');*/ window.open($(this).data('url')); });
 
-    // AJAX send
+
+    // ---- Typing effect ----
+    $("#lead-content h2").html("<div style='display: inline-block; border-right: 3px solid #a0cfee; padding-right: 10px;'></div>")
+    
+    // List of sentences
+    var _CONTENT = ["Senior Undergraduate, IIT Delhi", "Intelligence S/W Intern, Samsung", "Full-Stack Web Developer", "Machine Learning R&D"];
+
+    // Current sentence being processed
+    var _PART = 0;
+
+    // Character number of the current sentence being processed 
+    var _PART_INDEX = 0;
+
+    // Holds the handle returned from setInterval
+    var _INTERVAL_VAL;
+
+    // Element that holds the text
+    var _ELEMENT = $("#lead-content>h2>div");
+
+    // Implements typing effect
+    function Type() { 
+        var text =  _CONTENT[_PART].substring(0, _PART_INDEX + 1);
+        _ELEMENT.text(text);
+        _PART_INDEX++;
+
+        // If full sentence has been displayed then start to delete the sentence after some time
+        if(text === _CONTENT[_PART]) {
+            clearInterval(_INTERVAL_VAL);
+            setTimeout(function() {
+                _INTERVAL_VAL = setInterval(Delete, 20);
+            }, 1000);
+        }
+    }
+
+    // Implements deleting effect
+    function Delete() {
+        var text =  _CONTENT[_PART].substring(0, _PART_INDEX - 1);
+        _ELEMENT.text(text);
+        _PART_INDEX--;
+
+        // If sentence has been deleted then start to display the next sentence
+        if(text === '') {
+            clearInterval(_INTERVAL_VAL);
+
+            // If last sentence then display the first one, else move to the next
+            if(_PART == (_CONTENT.length - 1))
+                _PART = 0;
+            else
+                _PART++;
+            _PART_INDEX = 0;
+
+            // Start to display the next sentence after some time
+            setTimeout(function() {
+                _INTERVAL_VAL = setInterval(Type, 60);
+            }, 150);
+        }
+    }
+
+    // Start the typing effect on load
+    _INTERVAL_VAL = setInterval(Type, 60);
+
+
+
+    // ---- AJAX contact form send ----
     window.addEventListener("DOMContentLoaded", function() {
 
       // get the form elements defined in your form HTML above   
